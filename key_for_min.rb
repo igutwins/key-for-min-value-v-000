dd6e193277e9cff615c2dcc98fb0573b26714c28
Ian Gutwinski <iangutwinski@gmail.com>
2019-12-13 01:25:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  return nil if name_hash.empty?
-  collection = []
-  name_hash.collect do |key, var|
-    collection << var
+  lowest_key = nil
+  lowest_value = nil #just setting up the variables inside the method
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
   end
-#here i have an array of the values and need to sort them
-  sorted_collection = collection.sort
-
-#return the key for the value of the collection at 0, which is the min
-  name_hash.key(sorted_collection[0])
+end
+  lowest_key
 end
